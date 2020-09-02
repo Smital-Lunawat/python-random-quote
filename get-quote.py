6b3bd97b4f862bf645c2bac8cfca8b5dbf024af7
Smital-Lunawat <65792778+Smital-Lunawat@users.noreply.github.com>
2020-09-02 23:21:32 +0530

    Add files via upload
    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
 def main():
-   print("Keep it logically awesome.")
+   #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("/Users/suyash/Downloads/python-random-quote-master/quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = 13
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   main()
